c9f30f7c1fc1bbef2ba28c38c32412c4654a9293
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2022-11-28 13:34:11 +0100

    Profile (Name, Age, Birthyear)

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Nikolai";
+let lastName = "Niklaus";
+let fullName = firstName + ' ' + lastName;
+
+console.log("hello, my name is", fullName);
+
+let age = 29;
+let year = 2022;
+let birthYear = year - age;
+
+console.log(birthYear);
